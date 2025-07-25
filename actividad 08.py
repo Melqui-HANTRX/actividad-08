926276af8ccef476066f3e4d7d4e53db5b436aed
Melqui Garcia <monterrosomelqui39@gmail.com>
2025-07-25 09:48:21 -0600

    while true

--- actividad 08.py
+++ actividad 08.py
@@ -17,4 +17,19 @@ while True:
             print("Elemento guardado en su canasta.")
         case "2":
             print("Lista actual", producto)
-            valor_de_tienda =
+            valor_de_tienda = int(input("Ingrese el indice del elemento o el nombre, el indice comienza de (0): "))
+            nuevo_valor = input("Ingrese el nuevo producto: ")
+            producto[valor_de_tienda] = nuevo_valor
+            print("Producto modificado.")
+        case "3":
+            print("lista actual: ", producto)
+            valor = input("Ingrese el producto a eliminar (puede ser por indice o nombre): ")
+            producto.remove(valor)
+            print("El producto fue eliminado. ")
+        case "4":
+            print("Productos en su canasta actuales: ", producto)
+        case "5":
+            print("Saliendo del programa.....")
+            break
+        case _:
+            print("Opción invalida, intente de nuevo. ")
